14076f30ec64fab855bb9a8ee91fcdf6bdf5d839
Khinshan Khan <khanshan501@gmail.com>
2021-03-11 20:29:45 -0500

    added control buttons and selector

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Controls from "./components/Controls";
 import Table from "./components/Table";
 
 const App = () => {
+  const [rows, setRow] = useState(0);
+  const [cols, setCol] = useState(0);
+
+  const addRow = () => {};
+  const addCol = () => {};
+
+  const removeRow = () => {};
+  const removeCol = () => {};
+
+  const colorSelect = () => {};
+
+  const uncoloredFill = () => {};
+  const fill = () => {};
+  const clearAll = () => {};
+
   return (
     <>
       <h1>Grid Maker Reactified</h1>
-      <Controls />
+      <form>
+        <select id="color_choose" onChange="colorSelect">
+          <option value="white">White</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="purple">Purple</option>
+          <option value="#cdcdcd">Gray</option>
+          <option value="#F781F3">Pink</option>
+          <option value="#2EFEF7">Toothpaste</option>
+          <option value="#00FF00">Lime</option>
+          <option value="#2E9AFE">Light Blue</option>
+        </select>
+      </form>
+      <button onClick={addRow}>ADD ROW</button>
+      <button onClick={addCol}>ADD COL</button>
+
+      <button onClick={removeRow}>REMOVE ROW</button>
+      <button onClick={removeCol}>REMOVE COL</button>
+
+      <button onClick="uncoloredFill">FILL UNCOLORED</button>
+      <button onClick="fill">FILL ALL</button>
+      <button onClick="clearAll">CLEAR ALL</button>
       <br />
       <Table />
     </>
--- src/components/Controls/Controls.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Controls = () => {
-  return <>control</>;
-};
-
-export default Controls;
