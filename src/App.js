512db3daac22c5331de7e0239ed0b054c452ec18
Khinshan Khan <khanshan501@gmail.com>
2021-03-11 20:04:21 -0500

    renamed to separate things out

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <>hello there</>;
+  return (
+    <>
+      <h1>Grid Maker Reactified</h1>
+      controls
+      <br />
+      grid
+    </>
+  );
 };
 
 export default App;
