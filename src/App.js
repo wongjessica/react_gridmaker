0c6cb1c6c875e7db9f1fcc57a5784ea981d22548
Khinshan Khan <khanshan501@gmail.com>
2021-03-11 20:08:50 -0500

    created component files for others

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import Controls from "./components/Controls";
+import Table from "./components/Table";
 
 const App = () => {
   return (
     <>
       <h1>Grid Maker Reactified</h1>
-      controls
+      <Controls />
       <br />
-      grid
+      <Table />
     </>
   );
 };
